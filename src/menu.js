21e20394c4a666a7bcfc68b8491df765eb7adbcd
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2024-09-04 22:36:53 +0000

    Add menu item images

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,11 @@
 import fullStar from './assets/star.svg'
 import emptyStar from './assets/star-outline.svg'
+import dollarPancake from './assets/dollar-pancake.jpeg'
+import cheeseburger from './assets/cheeseburger.jpeg'
+import chickenClub from './assets/chicken-club.jpeg'
+import dinerFries from './assets/diner-fries.jpeg'
+import coleslaw from './assets/coleslaw.jpeg'
+import theValueMeal from './assets/the-value-meal.jpeg'
 
 export function buildMenuPage() {
     const content = document.getElementById('content')
@@ -10,31 +16,38 @@ export function buildMenuPage() {
             const menuItems = (function() {
                 const items = []
                 class Item {
-                    constructor(name, description, rating) {
+                    constructor(name, description, rating, image) {
                         this.name = name
                         this.description = description
                         this.rating = rating
+                        this.image = image
                     }
                 }
 
                 items.push(new Item('Dollar Pancake',
                                     'Fluffy buttermilk pancake with a drizzle of syrup',
-                                    2),
+                                    2,
+                                    dollarPancake),
                         new Item('Cheeseburger',
                                 'Juicy beef patty, melted American cheese, lettuce',
-                                1),
+                                1,
+                                cheeseburger),
                         new Item('Chicken Club',
                                 'Crispy chicken breast, bacon, lettuce, and mayo on whole wheat bread',
-                                3),
+                                3,
+                                chickenClub),
                         new Item('Diner Fries',
                                 'Crispy French fries cooked to a golden brown',
-                                1),
+                                1,
+                                dinerFries),
                         new Item('Coleslaw',
                                 'Creamy coleslaw made with shredded cabbage and a hint of vinegar',
-                                1),
+                                1,
+                                coleslaw),
                         new Item('The Value Meal',
                                 'A combo meal featuring a Chicken Club, Coleslaw, and a drink',
-                                0,)
+                                0,
+                                theValueMeal)
                             )
 
                 return items
@@ -89,6 +102,7 @@ export function buildMenuPage() {
                 const addImage = function() {
                     const img = document.createElement('img')
                     img.alt = 'photo of ' + item.name
+                    img.src = item.image
                     card.appendChild(img)
                 }
 
